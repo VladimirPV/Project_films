e665264454daf43b94f5e1d110eb4e5c333aec9b
Vladimir <ruspvve@gmail.com>
2020-06-01 23:18:14 +0400

    test commit

--- console.js
+++ console.js
@@ -1,2 +1,22 @@
 "use strict";
 
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    let num = 5;
+    console.log(num);
+}
+
+showFirstMessage("Hello World");
+console.log(num);
+
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(1, 1));
+console.log(calc(1, 2));
+console.log(calc(1, 3));
